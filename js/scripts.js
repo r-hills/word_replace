258f85a01d46a49354ea12c6eea8919beb8e9443
Rick Hills <rh-jobs@outlook.com>
2015-09-11 11:50:31 -0700

    It works.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 var wordReplace = function(inputString, wordToReplace, replacementWord) {
 
 	var regex = new RegExp("\\b" + wordToReplace + "\\b", "gi");
-	var outputString = inputString.replace(regex, replacementWord);
+	return inputString.replace(regex, replacementWord);
 
-	return outputString;
 };
 
 
