7008e266ccbb2e6c75c9f2a4b2db8b8abd1013e6
Rick Hills <rh-jobs@outlook.com>
2015-09-11 13:54:28 -0700

    Now with super-duper html

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,25 @@ var wordReplace = function(inputString, wordToReplace, replacementWord) {
 };
 
 
+$(document).ready(function() {
+	$("form#word-replace").submit(function(event) {
+
+		var inputString = $("input#string").val();
+		var wordToReplace = $("input#word").val();
+		var replacementWord = $("input#replacement").val();
+		var outputString = wordReplace(inputString, wordToReplace, replacementWord);
+
+		$("form#word-replace")[0].reset();
+
+		$(".input").text(inputString);
+		$(".output").text(outputString);
+
+		$("#result").show();
+
+		event.preventDefault();
+	});
+
+});
 
 
 
